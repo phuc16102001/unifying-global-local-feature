2a01a8459a3c58013cd67b372bebbcd2811d2364
phuc16102001 <phuc16102001@gmail.com>
2023-02-15 13:00:17 +0000

    Update script

--- train.sh
+++ train.sh
@@ -1,5 +1,5 @@
 export CUDA_VISIBLE_DEVICES=0
-python3 train_e2e.py "soccernetv2" \
+python3 train_e2e.py "soccernet" \
 	"/ext_drive/data/soccernet_720p_2fps" \
 	-s "results/save_soccernet" \
 	-m "rny008_gsm" \
@@ -7,7 +7,7 @@ python3 train_e2e.py "soccernetv2" \
 	--learning_rate 0.001 \
 	--num_epochs 150 \
 	--start_val_epoch 149 \
-	--temporal_arch "gru" \
+	--temporal_arch "former" \
 	--warm_up_epochs 3 \
 	--batch_size 4 \
 	--crop_dim -1 \
