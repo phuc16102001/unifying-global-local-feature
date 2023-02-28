f4777333bb5e5c55b39c5df1e06d7e4c86a6a3f8
phuc16102001 <phuc16102001@gmail.com>
2023-02-28 17:39:04 +0700

    Update test script

--- test.sh
+++ test.sh
@@ -4,6 +4,7 @@ python3 test_e2e.py "results/BCE_800MF_GRU" \
 	"/ext_drive/data/soccernet_720p_2fps" \
 	-s "test" \
 	--recall_thresh 0.5 \
+	--criterion_key "val" \
 	--save
 
 # python3 test_e2e.py <model_dir> <frame_dir> -s <split> --save
