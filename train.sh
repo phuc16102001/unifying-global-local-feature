9f80f5d8bf4ff8e5fe30b693885906c0c7b252f8
phuc16102001 <phuc16102001@gmail.com>
2023-03-14 00:09:15 +0700

    Update to train

--- train.sh
+++ train.sh
@@ -1,8 +1,8 @@
 export CUDA_VISIBLE_DEVICES=0,2
 python3 train_e2e.py "soccernet" \
 	"/ext_drive/data/soccernet_720p_2fps" \
-	-s "results/800MF_Former_NoGSM" \
-	-m "rny008" \
+	-s "results/800MF_Former" \
+	-m "rny008_gsm" \
 	-mgpu \
 	--learning_rate 0.0001 \
 	--num_epochs 150 \
@@ -10,8 +10,9 @@ python3 train_e2e.py "soccernet" \
 	--temporal_arch "former" \
 	--warm_up_epochs 3 \
 	--batch_size 10 \
+	--clip_len 140 \
 	--crop_dim -1 \
-	--label_type "integer" \
+	--label_type "one-hot" \
 	--num_workers 8 \
 	--mixup \
 
