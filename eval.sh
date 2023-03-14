35135db8c590901f84d33e723c8a7ce59761ee0f
phuc16102001 <phuc16102001@gmail.com>
2023-03-14 11:53:48 +0700

    Update dropout to output of transformer

--- eval.sh
+++ eval.sh
@@ -1,5 +1,5 @@
-python eval_soccernetv2.py "D:\data\SoccerNet-Pred\pred-test.100.recall.json" \
+python eval_soccernetv2.py "D:\data\SoccerNet-Pred\Integer\pred-test.17.recall.json" \
 	--split "test" \
 	--eval_dir "D:\data\SoccerNet-Pred\output" \
 	--soccernet_path "D:\data\SoccerNet" \
-    --nms_window 2
+	--nms_window 2
