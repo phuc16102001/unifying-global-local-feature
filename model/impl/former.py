87ce16054f2117df20347e38955d96f9df8e895d
phuc16102001 <phuc16102001@gmail.com>
2023-03-17 17:53:08 +0700

    Check attention result

--- model/impl/former.py
+++ model/impl/former.py
@@ -79,8 +79,9 @@ class EncoderLayer(nn.Module):
         
     def forward(self, x, mask=None, key_padding_mask=None):
         x_attn = self.attn(x, x, x, 
-                           attn_mask=mask, key_padding_mask=key_padding_mask)[1]
-        print(x_attn.size())
+                           attn_mask=mask, key_padding_mask=key_padding_mask)[0]
+        print(key_padding_mask)
+        print(x_attn)
         if (key_padding_mask is not None):
             x_attn = x_attn.masked_fill(key_padding_mask.unsqueeze(-1), 0)
         x = x + self.dropout_1(x_attn)
