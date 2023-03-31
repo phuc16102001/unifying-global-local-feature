8de6668862df1fec61f52914c1a32d16fa11e114
phuc16102001 <phuc16102001@gmail.com>
2023-03-31 22:31:37 +0700

    Print min max of tensor

--- model/impl/former.py
+++ model/impl/former.py
@@ -80,6 +80,7 @@ class EncoderLayer(nn.Module):
         
     def forward(self, x, mask=None, key_padding_mask=None):
         print(x)
+        print(torch.min(x), torch.max(x))
         x_attn = self.attn(x, x, x, attn_mask=mask, key_padding_mask=key_padding_mask)[0]
         cnt_nan = torch.sum(torch.isnan(x_attn))
         print("After attn", cnt_nan)
