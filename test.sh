bc1a0fe2472904b975a9c3ee52d901ee2fcedc8a
DoVuongPhuc <phuc16102001@gmail.com>
2023-04-22 20:35:10 +0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,12 +1,10 @@
-
 export CUDA_VISIBLE_DEVICES=0
-python3 eval_soccernetv2.py "/Users/JLJXDXCFK7/Prediction/pred-test.149.recall.json" \
-	--split "test" \
-	--eval_dir "/Users/JLJXDXCFK7/Prediction/output" \
-	--soccernet_path "/Users/JLJXDXCFK7/Data/soccernet-label" \
-	--nms_window 2 \
-	--filter_score 0.05 \
-	--allow_remove
+python3 test_e2e.py "results/BCE_800MF_GRU" \
+	"/ext_drive/data/soccernet_720p_2fps" \
+	-s "test" \
+	--recall_thresh 0.5 \
+	--criterion_key "val" \
+	--save
 
 # python3 test_e2e.py <model_dir> <frame_dir> -s <split> --save
 # default using map metric
