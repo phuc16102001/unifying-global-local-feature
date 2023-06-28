aade3869a25ebf89b8920a006f27bcd0652e1fce
DoVuongPhuc <phuc16102001@gmail.com>
2023-06-28 21:08:25 +0700

    Delete eval.sh

--- eval.sh
+++
@@ -1,7 +0,0 @@
-python3 eval_soccernetv2.py "/Users/JLJXDXCFK7/Prediction/pred-test.149.recall.json" \
-	--split "test" \
-	--eval_dir "/Users/JLJXDXCFK7/Prediction/output" \
-	--soccernet_path "/Users/JLJXDXCFK7/Data/soccernet-label" \
-	--nms_window 2 \
-	--filter_score 0.05 \
-	--allow_remove
